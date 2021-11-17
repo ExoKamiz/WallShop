e0429072f6520bc90f9bfce92bad172cd91bf435
Максим Зимак <mzimak18@gmail.com>
2021-11-17 18:37:00 +0100

    go to push

---
+++ WallShop/Controllers/CategoryController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WallShop.Data;
+using WallShop.Models;
+
+namespace WallShop.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db; //дает доступ к базе данных в обекте _db
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Category; //создаем список типа IEnumerable обьектов Category и присваем обьект для получения катигории из нашей бд
+            return View(objList);
+        }
+
+        //GET - CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj) //тут получаем обьект который нужно добавить в бд
+        {
+            _db.Category.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index"); //перенаправление исполнение кода в метод Index
+        }
+    }
+}
--- WallShop/Models/Category.cs
+++ WallShop/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ namespace WallShop.Models
 
         public string Name { get; set; }
 
+        [DisplayName("Display Oreder")]  //для разделения слова
         public int DisplayOrder { get; set; }
     }
 }
